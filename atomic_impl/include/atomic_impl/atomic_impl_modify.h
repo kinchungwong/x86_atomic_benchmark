1333f08cb3eea66ef045a18487d0a7c0d1370f1e
kinchungwong <kinchungwong@users.noreply.github.com>
2025-04-04 09:00:06 -0700

    simplified modify() design
    - Op can be function or callable object.
    - Op is now unary, takes the loaded value as input.
    - Op can be stateful; states are visible to Op's owner.
    - traits header file not needed; removed.
    - Always return both loaded and computed values as a pair.
    - Updated simple_demo.

--- atomic_impl/include/atomic_impl/atomic_impl_modify.h
+++ atomic_impl/include/atomic_impl/atomic_impl_modify.h
@@ -2,68 +2,59 @@
 #define ATOMIC_IMPL_MODIFY_H
 
 #include <cstdint>
+#include <type_traits>
+#include <utility>
 #include "atomic_impl/atomic_impl_defs.h"
 #include "atomic_impl/atomic_impl_methods.h"
-#include "atomic_impl/atomic_impl_traits.h"
 #include "atomic_impl/atomic_impl_busywait.h"
 
 namespace atomic_impl {
 
 /**
- * @brief Modifies the value of the destination by applying the binary operation
- *        to (dest, operand) and storing the result in dest.
- *
- * @details This function uses a compare-and-swap loop to ensure that the
- *          read-modify-write steps happened without interference from
- *          other threads operating on the same destination.
- *
- * @remarks This function returns the value of the destination after the operation.
- *          If, instead, the user wants to obtain the destination value immediately
- *          before the successful operation, the user should use fetch_modify().
- *
- * @see add, sub, and, or, xor, nand
- * 
- * @see fetch_modify, fetch_add, fetch_sub, fetch_and, fetch_or, fetch_xor, fetch_nand
- *
- * @tparam Op The type of the binary operation to be applied. This must be a callable
- *            object that takes two arguments (load(dest), operand) and returns a value
- *           that can be assigned back to dest. Its operator() must be const-qualified.
- *
- * @note Generally speaking, Op should be stateless (and satisfied default constructible
- *       and trivial copyable), but currently this is not enforced.
- *
- * @note For one of the six basic operations, consider using the corresponding function:
- *       add, sub, and, or, xor, nand, or their fetch_modify variants, for better
- *       performance.
+ * @brief Performs a read-modify-write operation on a destination value, where
+ *        the new value is computed with the provided callable object.
  *
  * @param dest The destination value to be modified.
  *
- * @param operand The operand to be used in the operation.
+ * @param op A callable object that accepts a value loaded from the destination,
+ *           and returns a new value to be written back to the destination.
+ *           This object can be stateful; state changes are visible to the caller.
  *
- * @param op The binary operation to be applied. 
+ * @returns The (old, new) pair of values from a successful operation.
  *
- * @return The value of the destination after the operation.
+ * @remark This function uses a compare-and-swap loop to ensure that the
+ *         read-modify-write steps happened without interference from
+ *         other threads operating on the same destination.
+ *
+ * @remark For one of the six basic operations, consider using the corresponding function:
+ *         add, sub, and, or, xor, nand, or their fetch_modify variants, for better
+ *         performance.
+ *
+ * @see fetch_modify, fetch_add, fetch_sub, fetch_and, fetch_or, fetch_xor, fetch_nand
  */
 template <class Op>
 static
 FLATTEN
-uintptr_t
+std::pair<uintptr_t, uintptr_t>
 INLINE_NEVER
-modify(uintptr_t& dest, uintptr_t operand, const Op& op)
+modify(uintptr_t& dest, Op& op)
 {
-    static_assert(traits::is_binary_op<Op>::value, "Op must be a binary operation");
+    using op_result_type = decltype(op(std::declval<uintptr_t>()));
+    static_assert(std::is_assignable<uintptr_t&, op_result_type>::value,
+                  "Op invocation must be result compatible");
     detail::busywaiter waiter;
     while (true)
     {
-        uintptr_t expected = load(dest);    
-        uintptr_t desired = op(expected, operand);
-        if (try_replace(dest, expected, desired))
+        //! @note reload from dest is required after a wait().
+        uintptr_t loaded = load(dest);
+        uintptr_t computed = op(loaded);
+        uintptr_t expected_or_actual = loaded;
+        if (try_replace(dest, expected_or_actual, computed))
         {
-            return desired;
+            return std::make_pair(loaded, computed);
         }
         waiter.wait();
-        //! @note after a busywait pause or yield, dest needs reloaded.
-    };
+    }
     //! @note this line shall be unreachable.
 };
 
--- main/main.cpp
+++ main/main.cpp
@@ -7,23 +7,20 @@ void simple_demo()
 {
     uintptr_t dest = 3;
     uintptr_t value = 555;
-    struct SquareModuloMillion
+    auto square_modulo_million = [=](uintptr_t current)
     {
-        uintptr_t operator()(uintptr_t dest, uintptr_t value) const
+        // Use 64-bit so that the multiplication step
+        // doesn't overflow.
+        uint64_t result = current;
+        for (size_t i = 0; i < value; ++i)
         {
-            // Use 64-bit so that the multiplication step
-            // doesn't overflow.
-            uint64_t current = dest;
-            size_t count = value;
-            for (size_t i = 0; i < value; ++i)
-            {
-                current = (current * current) % (uint64_t)1000000;
-            }
-            return static_cast<uintptr_t>(current);
+            result = (result * result) % (uint64_t)1000000;
         }
+        return static_cast<uintptr_t>(result);
     };
-    auto fetched = atomic_impl::modify(dest, value, SquareModuloMillion());
-    std::cout << "Result: " << fetched << std::endl;
+    auto result = atomic_impl::modify(dest, square_modulo_million);
+    std::cout << "Before: " << result.first << std::endl;
+    std::cout << "After: " << result.second << std::endl;
 }
 
 int main()
