9336a366be119e99847b2785eef6e2e8da6ed5fd
kinchungwong <kinchungwong@users.noreply.github.com>
2025-04-06 12:48:50 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
+INCLUDE_DIR="-I."
+INCLUDE_DIR="-I./main"
+INCLUDE_DIR="${INCLUDE_DIR} -I./atomic_impl/include"
+INCLUDE_DIR="${INCLUDE_DIR} -I./atomic_impl/src"
+INCLUDE_DIR="${INCLUDE_DIR} -I./nano_timer/include"
+INCLUDE_DIR="${INCLUDE_DIR} -I./nano_timer/src"
+
 mkdir -p ./build
-g++ -std=c++11 -g -O3 -DNDEBUG -o ./build/main.out ./main/main.cpp -I. -I./atomic_impl/include -I./atomic_impl/src -fPIC -flto
+g++ -std=c++11 -g -O3 -DNDEBUG -o ./build/main.out ./main/main.cpp ${INCLUDE_DIR} -fPIC -flto
 objdump -d -C -M intel ./build/main.out > ./build/main.s
